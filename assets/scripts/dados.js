9335dc2b10338c1df23f74675ff9a3f793b433c0
Thiago Resmini Slongo <rthiagoslongo@gmail.com>
2026-07-19 10:23:24 -0300

    feat: ajuste da funcao map, e criado o local storage

--- assets/scripts/dados.js
+++ assets/scripts/dados.js
@@ -1,7 +1,7 @@
 import { Vaga, VagaFrontEnd } from './motor.js';
 
-async function buscarVagas(){
-    try { 
+async function buscarVagas() {
+    try {
         console.log("Carregando Vagas... Aguardo um momento!");
         let resposta = await fetch('./assets/dados/vagas.json');
         if (!resposta.ok) {
@@ -11,11 +11,27 @@ async function buscarVagas(){
         if (!data || data.length === 0) {
             console.error("Buscando vagas ... Nenhuma vaga encontrada ", data);
         }
-        console.log(data)
+        const vagaDados = data.map((vagaJson) => {
+            if (vagaJson.nivelMinimo) {
+                return new VagaFrontEnd(vagaJson.empresa, vagaJson.cargo, vagaJson.requisitos, vagaJson.salario, vagaJson.modalidade, vagaJson.nivelMinimo)
+            } else {
+                return new Vaga(vagaJson.empresa, vagaJson.cargo, vagaJson.requisitos, vagaJson.salario, vagaJson.modalidade)
+            }
+        })
+
+        return vagaDados;
+
     } catch (retorno) {
         console.error("Não há vagas disponíveis. Tente de novo", retorno);
-    }    
+    }
 }
 
-buscarVagas();
+function carregaCandidato() {
+    return JSON.parse(localStorage.getItem('dadosCandidato'));
+}
 
+function salvarCandidato(dadosCandidato) {
+    localStorage.setItem('dadosCandidato', JSON.stringify(dadosCandidato));
+}
+
+export { buscarVagas, carregaCandidato, salvarCandidato }
